71f2193bce5057e11911ea04ed856a1a781eef2e
Nortess1 <119341569+Nortess1@users.noreply.github.com>
2024-01-27 17:44:11 +0100

    Wwise objects SFX

--- GGJ2024/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ GGJ2024/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,25 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_SFX_COMMUN_GRAB = 1306196216U;
+        static const AkUniqueID PLAY_SFX_COUSSIN_GRAB = 2726606975U;
+        static const AkUniqueID PLAY_SFX_COUSSIN_IMPACT = 503920167U;
+        static const AkUniqueID PLAY_SFX_COUSSIN_THROW = 1947971867U;
+        static const AkUniqueID PLAY_SFX_DEATH_ELECTRICITY = 3895751602U;
+        static const AkUniqueID PLAY_SFX_DILDO_GRAB = 1223086071U;
+        static const AkUniqueID PLAY_SFX_DILDO_IMPACT = 2289151103U;
+        static const AkUniqueID PLAY_SFX_DILDO_SLASH = 2762880728U;
+        static const AkUniqueID PLAY_SFX_DILDO_THROW = 3088255043U;
+        static const AkUniqueID PLAY_SFX_EPEE_GRAB = 2246561112U;
+        static const AkUniqueID PLAY_SFX_EPEE_IMPACT = 1623677600U;
+        static const AkUniqueID PLAY_SFX_EPEE_SLASH = 533704501U;
+        static const AkUniqueID PLAY_SFX_EPEE_THROW = 747977262U;
+        static const AkUniqueID PLAY_SFX_LOUPE_BREAK = 358670559U;
+        static const AkUniqueID PLAY_SFX_LOUPE_GRAB = 2235098404U;
+        static const AkUniqueID PLAY_SFX_LOUPE_IMPACT = 378057844U;
+        static const AkUniqueID PLAY_SFX_LOUPE_THROW = 2764374482U;
+        static const AkUniqueID PLAY_SFX_PAPIER_GRAB = 1406297036U;
+        static const AkUniqueID PLAY_SFX_PAPIER_THROW = 706616106U;
         static const AkUniqueID TEST = 3157003241U;
     } // namespace EVENTS
 
