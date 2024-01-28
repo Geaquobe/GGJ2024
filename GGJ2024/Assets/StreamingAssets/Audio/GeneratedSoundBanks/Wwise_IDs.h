1b112e8cacf1516033cffdd45843a944fe3f2b21
Nortess1 <119341569+Nortess1@users.noreply.github.com>
2024-01-28 01:11:41 +0100

    Wwise new objects

--- GGJ2024/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ GGJ2024/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,8 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_SFX_ASSOMMER = 383418027U;
+        static const AkUniqueID PLAY_SFX_BODYFALL = 2047017905U;
         static const AkUniqueID PLAY_SFX_COMMUN_GRAB = 1306196216U;
         static const AkUniqueID PLAY_SFX_COUSSIN_GRAB = 2726606975U;
         static const AkUniqueID PLAY_SFX_COUSSIN_IMPACT = 503920167U;
@@ -22,6 +24,8 @@ namespace AK
         static const AkUniqueID PLAY_SFX_DILDO_IMPACT = 2289151103U;
         static const AkUniqueID PLAY_SFX_DILDO_SLASH = 2762880728U;
         static const AkUniqueID PLAY_SFX_DILDO_THROW = 3088255043U;
+        static const AkUniqueID PLAY_SFX_DRINK = 185603492U;
+        static const AkUniqueID PLAY_SFX_EAT = 1711751912U;
         static const AkUniqueID PLAY_SFX_EPEE_GRAB = 2246561112U;
         static const AkUniqueID PLAY_SFX_EPEE_IMPACT = 1623677600U;
         static const AkUniqueID PLAY_SFX_EPEE_SLASH = 533704501U;
@@ -32,8 +36,33 @@ namespace AK
         static const AkUniqueID PLAY_SFX_LOUPE_THROW = 2764374482U;
         static const AkUniqueID PLAY_SFX_PAPIER_GRAB = 1406297036U;
         static const AkUniqueID PLAY_SFX_PAPIER_THROW = 706616106U;
-        static const AkUniqueID TEST = 3157003241U;
+        static const AkUniqueID PLAY_SFX_PISTOLET_GRAB = 4037183997U;
+        static const AkUniqueID PLAY_SFX_PISTOLET_IMPACT = 2958712709U;
+        static const AkUniqueID PLAY_SFX_PISTOLET_SHOT = 2562833167U;
+        static const AkUniqueID PLAY_SFX_PISTOLET_THROW = 2404878845U;
+        static const AkUniqueID PLAY_SFX_TAPIS_CUT = 879869776U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace VISION
+        {
+            static const AkUniqueID GROUP = 4026346633U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ADULT = 2752733175U;
+                static const AkUniqueID BABY = 1543097833U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace VISION
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RTPC_SLIDER_VOLUME = 3461965729U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
@@ -41,7 +70,6 @@ namespace AK
         static const AkUniqueID AMB = 1117531639U;
         static const AkUniqueID FOLEY = 247557814U;
         static const AkUniqueID SFX = 393239870U;
-        static const AkUniqueID TEST = 3157003241U;
         static const AkUniqueID UI = 1551306167U;
         static const AkUniqueID VOICE = 3170124113U;
     } // namespace BANKS
